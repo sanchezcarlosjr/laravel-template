6ad8ef77ae8b53e9c531e2d18cfce509fc70cf93
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2021-08-11 09:58:42 -0700

    chore(contact-us): show a message

--- html/resources/js/entry.component.vue
+++ html/resources/js/entry.component.vue
@@ -9,7 +9,7 @@ import Component from "vue-class-component"
 @Component
 export default class EntryComponent extends Vue {
     mounted() {
-        console.log('%c Interested in working with us?\n See https://www.sanchezcarlosjr.com', 'background: #222; color: #bada55');
+        console.log('%c Are you interested in working with us?\n Send me a message on https://www.sanchezcarlosjr.com (I\'m Carlos Eduardo Sanchez Torres, an UABC Alumni)', 'background: #222; color: #bada55');
     }
 }
 </script>
