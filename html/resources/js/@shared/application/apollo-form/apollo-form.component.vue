05b20ccb55cab1acdc1ea7e5441eaee3a425cdaf
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2021-09-18 17:43:16 -0700

    chore(modal loading): better user experience

--- html/resources/js/@shared/application/apollo-form/apollo-form.component.vue
+++ html/resources/js/@shared/application/apollo-form/apollo-form.component.vue
@@ -1,9 +1,11 @@
 <template>
-    <vue-form-generator
-      ref="form"
-      :schema="schema.schema"
-      :model="model"
-    />
+    <b-overlay :show="busy" rounded="sm" style="width: 100%">
+        <vue-form-generator
+          ref="form"
+          :schema="schema.schema"
+          :model="model"
+        />
+    </b-overlay>
 </template>
 
 <script src="./apollo-form.ts" lang="ts"></script>
